20ffed8a467d9f36d5e22add9ae4f4902fbfb12f
Toms <tomsterauds@gmail.com>
2024-05-10 11:29:18 +0300

    home page infinite scroll added

--- src/components/home/HomeRecipeManager.vue
+++ src/components/home/HomeRecipeManager.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { useApiFetch } from '@/composables/apiFetch'
 import { useGetUrl } from '@/composables/getUrl'
-import { watch } from 'vue'
+import { watch, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-import { useLocalStorage } from '@vueuse/core'
+import { useLocalStorage, useInfiniteScroll } from '@vueuse/core'
+
 const { locale } = useI18n()
+const el = ref(null)
+
 
 const homeApiDataStorage = useLocalStorage(
   'home-api-data-storage',
@@ -28,10 +31,19 @@ function createsEmptyObjArray() {
   }
 }
 
+
+useInfiniteScroll(
+  el,
+  () => {
+    newUrlCallback()
+  },
+  { distance: 10 }
+)
 </script>
 
 <template>
-  <button @click="newUrlCallback">Next Page</button>
+
+  <!-- <button @click="newUrlCallback">Next Page</button> -->
 
   <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
   <div v-else-if="homeApiDataStorage">
@@ -39,17 +51,26 @@ function createsEmptyObjArray() {
       <hr>
       <h2> page: {{ id + 1 }}</h2>
       <div v-for="recipe in page.results" :key="recipe.id">
-        {{ recipe.title }}
-
+        {{ recipe.title }} <br>
+        {{ recipe.url }}
         <div v-if="recipe.images && recipe.images.length > 0">
           <div v-for="(image, index) in recipe.images" :key="index">
             <img :src="image.generate_presigned_url_for_image" alt="Recipe Image" />
           </div>
         </div>
-
-
-
       </div>
     </div>
   </div>
+  <div ref="el"></div>
 </template>
+
+<style scoped>
+/* Adjust the size of the image */
+img {
+  width: 100%;
+  /* Set the width to 100% of its container */
+  height: auto;
+  /* Maintain the aspect ratio */
+  /* You can also specify specific width and height values */
+}
+</style>
