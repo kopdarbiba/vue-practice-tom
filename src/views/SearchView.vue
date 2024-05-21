44d166f5c5dd15d7010c90810a52ab53ec6aeec1
Toms <tomsterauds@gmail.com>
2024-05-21 21:10:22 +0300

    search page - small restructure|rename

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,7 +1,37 @@
 <script setup>
-import SearchPageManager from '@/components/search/SearchPageCore.vue'
+import OrderSelector from '@/components/search/filters/OrderSelector.vue'
+import SearchQueryBox from '@/components/search/filters/SearchQueryBox.vue'
+import MinPrice from '@/components/search/filters/MinPrice.vue'
+import MaxPrice from '@/components/search/filters/MaxPrice.vue'
+import CheckBoxeFieldLooper from '@/components/search/CheckBoxeFieldLooper.vue'
+import SearchRecipeManager from '@/components/search/SearchRecipeManager.vue'
 </script>
 
 <template>
-  <SearchPageManager />
+  <div id="search-page">
+
+    <div>
+      <br>
+      <OrderSelector />
+      <br>
+      <SearchQueryBox />
+      <br>
+      <MinPrice />
+      <MaxPrice />
+      <br>
+      <CheckBoxeFieldLooper />
+    </div>
+    <div>
+      <SearchRecipeManager />
+    </div>
+  </div>
 </template>
+
+
+<style scoped>
+#search-page {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+
+}
+</style>
